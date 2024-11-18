cadea2612583a6ec2a2a60094342575649cc37b5
lainconn <playforest99@gmail.com>
2024-11-18 03:57:01 +0300

    add image

--- hackaton-3d/vite.config.ts
+++ hackaton-3d/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '192.168.1.5',
+    port: 4000,
+  },
   plugins: [react()],
 })
